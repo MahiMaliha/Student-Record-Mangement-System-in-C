642a34ad72d1df8dd30a5516d67ebaf25109e2c8
Mahi Maliha <87617184+MahiMaliha@users.noreply.github.com>
2022-09-19 18:22:02 +0600

    Used macro

--- record.c
+++ record.c
@@ -2,3 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+//Used macro
+#define MAX_YR  9999
+#define MIN_YR  1900
+#define MAX_SIZE_USER_NAME 30
+#define MAX_SIZE_PASSWORD  20
+#define FILE_NAME  "studentRecordSystem.bin"
