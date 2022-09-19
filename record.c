38f97e314c493957b939db400d732d57f0fd49d9
Mahi Maliha <87617184+MahiMaliha@users.noreply.github.com>
2022-09-19 18:23:51 +0600

    elements of structure

--- record.c
+++ record.c
@@ -24,3 +24,12 @@ typedef struct
     char username[MAX_SIZE_USER_NAME];
     char password[MAX_SIZE_PASSWORD];
 } sFileHeader;
+//Elements of structure
+typedef struct// to call in program
+{
+    unsigned int student_id; // declare the integer data type
+    char fatherName[MAX_FATHER_NAME];// declare the charecter data type
+    char studentName[MAX_STUDENT_NAME];// declare the character data type
+    char studentAddr[MAX_STUDENT_ADDRESS];// declare the character data type
+    Date studentJoiningDate;// declare the integer data type
+} s_StudentInfo;
